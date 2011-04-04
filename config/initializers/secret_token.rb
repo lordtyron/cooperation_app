8eadede72597dedeef139dd44db4955962192a47
titopa <yamatokusanagi@gmail.com>
2011-04-04 14:38:03 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CooperationSystemUser::Application.config.secret_token = '9877a194dfbdf20528d17b95b8437d532b93a34a4c10f3a0fadf0da6928fd8417689a1bfeba0c30978b435a00ec35802ec384bdc1ae48c979972b7e69b79d8cd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+CooperationSystemUser::Application.config.session_store :cookie_store, :key => '_cooperation_system_user_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# CooperationSystemUser::Application.config.session_store :active_record_store
